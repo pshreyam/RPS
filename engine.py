925d5a4a13968d4fe05da4296e12402acd613bcd
Shreyam Pokharel <pshreyam@gmail.com>
2024-04-14 22:34:18 +0545

    Refactor and add tests
    Get started with pytest

--- engine.py
+++ engine.py
@@ -15,20 +15,29 @@ class Result(str, Enum):
     TIE = "Tie."
 
 
-def play(user_move: str) -> Tuple[str, str]:
-    available_moves = [Move.ROCK, Move.SCISSORS, Move.PAPER]
-    computer_move = random.choice(available_moves)
+AVAILABLE_MOVES = [Move.ROCK, Move.SCISSORS, Move.PAPER]
 
+
+def generate_computer_move() -> Move:
+    computer_move = random.choice(AVAILABLE_MOVES)
+    return computer_move
+
+
+def compute_result(user_move: Move, computer_move: Move):
     if user_move == computer_move:
-        return Result.TIE, computer_move
-
-    result = Result.LOSS
+        return Result.TIE
 
     if (
         (user_move == Move.ROCK and computer_move == Move.SCISSORS) or
         (user_move == Move.PAPER and computer_move == Move.ROCK) or
         (user_move == Move.SCISSORS and computer_move == Move.PAPER)
     ):
-        result = Result.WIN
+        return Result.WIN
 
+    return Result.LOSS
+
+
+def play(user_move: str) -> Tuple[str, str]:
+    computer_move = generate_computer_move()
+    result = compute_result(user_move=user_move, computer_move=computer_move)
     return result, computer_move
---
+++ tests/test_engine.py
@@ -0,0 +1,20 @@
+from engine import generate_computer_move, AVAILABLE_MOVES, compute_result, Move, Result
+
+
+def test_generate_computer_move():
+    computer_move = generate_computer_move()
+    assert computer_move in AVAILABLE_MOVES
+
+
+def test_compute_result():
+    assert compute_result(user_move=Move.ROCK, computer_move=Move.ROCK) == Result.TIE
+    assert compute_result(user_move=Move.ROCK, computer_move=Move.PAPER) == Result.LOSS
+    assert compute_result(user_move=Move.ROCK, computer_move=Move.SCISSORS) == Result.WIN
+
+    assert compute_result(user_move=Move.PAPER, computer_move=Move.ROCK) == Result.WIN
+    assert compute_result(user_move=Move.PAPER, computer_move=Move.PAPER) == Result.TIE
+    assert compute_result(user_move=Move.PAPER, computer_move=Move.SCISSORS) == Result.LOSS
+
+    assert compute_result(user_move=Move.SCISSORS, computer_move=Move.ROCK) == Result.LOSS
+    assert compute_result(user_move=Move.SCISSORS, computer_move=Move.PAPER) == Result.WIN
+    assert compute_result(user_move=Move.SCISSORS, computer_move=Move.SCISSORS) == Result.TIE
