fcbe0ba7d161c1b15be9e59dc38b137c58174028
Shreyam Pokharel <pshreyam@gmail.com>
2024-04-14 23:02:43 +0545

    Add intentional mistake for test failing

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -11,7 +11,7 @@ def test_compute_result():
     assert compute_result(user_move=Move.ROCK, computer_move=Move.PAPER) == Result.LOSS
     assert compute_result(user_move=Move.ROCK, computer_move=Move.SCISSORS) == Result.WIN
 
-    assert compute_result(user_move=Move.PAPER, computer_move=Move.ROCK) == Result.WIN
+    assert compute_result(user_move=Move.PAPER, computer_move=Move.ROCK) == Result.LOSS
     assert compute_result(user_move=Move.PAPER, computer_move=Move.PAPER) == Result.TIE
     assert compute_result(user_move=Move.PAPER, computer_move=Move.SCISSORS) == Result.LOSS
 
